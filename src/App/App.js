9e3fa1f2d41de840b50f4d02751d9c606d59e79a
Beth <bethhnielsen56@gmail.com>
2020-06-13 23:06:12 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>Top 100 Albums</h1>
+          <button className="btn btn-primary"><i className="fas fa-record-vinyl"></i>Testing Button</button>
+      </div>
+    );
+  }
+}
+
+export default App;
