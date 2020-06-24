2ed55c0ca2cece4264ed903f4f492803d296dc72
Beth <bethhnielsen56@gmail.com>
2020-06-23 20:47:44 -0500

    you are able to login and logout

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,53 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
+import fbConnection from '../helpers/data/connections';
+
+import Auth from '../components/Auth/Auth';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
 import AlbumsContainer from '../components/AlbumsContainer/AlbumsContainer';
-import SingleAlbumContainer from '../components/SingleAlbumContainer/SingleAlbumContainer';
 import Footer from '../components/Footer/Footer';
 
 import './App.scss';
 
+fbConnection();
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
+    const loadComponent = () => {
+      let componentToLoad = '';
+      if (authed) {
+        componentToLoad = <AlbumsContainer />;
+      } else {
+        componentToLoad = <Auth />;
+      }
+      return componentToLoad;
+    };
+
     return (
       <div className="App">
-        <MyNavbar />
-        <AlbumsContainer />
-        <SingleAlbumContainer />
+        <MyNavbar authed={authed}/>
+        {loadComponent()}
         <Footer />
       </div>
     );
